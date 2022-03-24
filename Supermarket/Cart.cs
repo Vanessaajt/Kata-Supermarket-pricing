e29b5807c734699333c11f983b8f3b8bd877a46f
vanessa ALI-TOLMBAYE <vanessa.tolmbaye@hotmail.fr>
2022-03-24 14:16:40 +0100

    Add Entities and theirs attributes

---
+++ Supermarket/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    public class Cart
+    {
+        public List<Product> products = new List<Product>();
+    }      
+}
---
+++ Supermarket/PricingRules.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    public enum PricingRules
+    {
+        SimplePrice=0,
+        ThreeForSinglePrice = 1,
+        TwoAndOneFree = 2,
+        PoundsToOunces = 3
+    }
+
+}
